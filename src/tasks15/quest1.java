8b9e68583da33a45e644a97696dfff73604d90ae
amrutha-pm <amruthanambiarmykeel@gmail.com>
2024-09-20 16:44:25 +0530

    initial cmt

--- src/tasks15/quest1.java
+++ src/tasks15/quest1.java
@@ -1,9 +1,15 @@
 package tasks15;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class quest1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+WebDriver driver=new ChromeDriver();
+driver.get("https://www.google.com");
+System.out.println(driver.getTitle());
 
 	}
 
